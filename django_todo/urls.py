71cbc72dfd2c2a17212e8a6158213e64e2b096a1
yt2b <reinventing.the.wheels@gmail.com>
2025-10-20 15:19:26 +0900

    feat: ログイン、ログアウト処理を実装

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("", include("todo.urls")),
 ]
