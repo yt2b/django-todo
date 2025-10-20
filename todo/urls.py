563c83a2312bd5a8bf2ce2207a79a67a45c653a8
yt2b <reinventing.the.wheels@gmail.com>
2025-10-20 20:38:12 +0900

    feat: サインアップ機能を実装

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todo.views import TaskListView
+from todo.views import TaskListView, SignupView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("signup/", SignupView.as_view(), name="signup"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,21 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
 
 
 class TaskListView(LoginRequiredMixin, TemplateView):
     template_name = "todo/index.html"
+
+
+class SignupView(CreateView):
+    template_name = "registration/signup.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        return response
