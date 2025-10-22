baf5bbeecd8fe7a8f186fcbeaf1a75ab1929fd7f
yt2b <reinventing.the.wheels@gmail.com>
2025-10-22 15:57:15 +0900

    feat: タスクを表示させる

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from todo.views import TaskListView, SignupView
+from todo.views import TaskListView, TaskToggleView, TaskDeleteView, SignupView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("signup/", SignupView.as_view(), name="signup"),
+    path("toggle/<int:id>", TaskToggleView.as_view(), name="toggle"),
+    path("delete/<int:id>", TaskDeleteView.as_view(), name="delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,43 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, DeleteView
+from django.views import View
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
+from todo.models import Task
 
 
 class TaskListView(LoginRequiredMixin, TemplateView):
     template_name = "todo/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = Task.objects.filter(user=self.request.user)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(user=request.user, title=title)
+        return redirect("index")
+
+
+class TaskToggleView(View):
+    def post(self, request, *args, **kwargs):
+        task = Task.objects.get(id=kwargs["id"])
+        print(task)
+        task.completed = not task.completed
+        task.save()
+        return redirect("index")
+
+
+class TaskDeleteView(View):
+    def post(self, request, *args, **kwargs):
+        task = Task.objects.get(id=kwargs["id"])
+        task.delete()
+        return redirect("index")
 
 
 class SignupView(CreateView):
