20bc18df5c2c964c314ee27a2e4ad1f3043c92bb
yt2b <reinventing.the.wheels@gmail.com>
2025-10-23 11:49:00 +0900

    feat: テストを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "testpassword"
+        self.user = User.objects.create_user(
+            username=self.username, password=self.password
+        )
+        self.client = Client()
+
+    def test_login_and_logout(self):
+        # ログインページにリダイレクトされる
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 302)
+        # ログイン
+        login_result = self.client.login(username=self.username, password=self.password)
+        self.assertTrue(login_result)
+        # ログイン後のページにアクセス
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "testuser のTODOリスト")
+        # ログアウト
+        response = self.client.post(reverse("logout"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "ログアウトしました")
